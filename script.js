ca7741daab4e12de27d32b85b739d226e4f9adc4
karemcode <karimchu1010@gmail.com>
2023-08-30 20:55:57 +0200

    Add event listener to handle key presses

--- script.js
+++ script.js
@@ -22,3 +22,25 @@ function calculate() {
 function reset() {
     display.value = "";
   }
+
+
+  // Add event listener to handle key presses
+document.addEventListener('keydown', function(event) {
+    if (event.key >= 0 && event.key <= 9) {
+      addToDisplay(event.key);
+    } else if (event.key === '+' || event.key === '-' || event.key === '*' || event.key === '/') {
+      addToDisplay(event.key);
+    } else if (event.key === 'Enter') {
+      event.preventDefault();
+      calculate();
+    } else if (event.key === 'Escape') {
+      reset();
+    } else if (event.key === 'Backspace') {
+      event.preventDefault();
+      display.value = display.value.slice(0, -1);
+    } else if (event.key === 'p') {
+      addToDisplay(Math.PI);
+    } else if (event.key === 's') {
+      calculateSquareRoot();
+    }
+  });

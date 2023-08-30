1842e75446426e5201ebe9fcfe54a05310541dfb
karemcode <karimchu1010@gmail.com>
2023-08-30 20:52:25 +0200

    Function to perform calculation

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ function addToDisplay(value) {
   display.value += value;
 }
 
+// Function to perform calculation
+function calculate() {
+    // Check if the value contains a percentage
+    if (display.value.includes('%')) {
+      const [num, percentage] = display.value.split('%');
+      display.value = parseFloat(num) * (parseFloat(percentage) / 100);
+    } else {
+      display.value = eval(display.value);
+    }
+  }
+
+  
